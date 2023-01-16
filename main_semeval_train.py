37ff524de9e03623d7ace69a14c6cc8b0a4a95f2
Abel Corrêa Dias <phd.abel@gmail.com>
2023-01-16 19:21:21 -0300

    fixing checkpoint load

--- main_semeval_train.py
+++ main_semeval_train.py
@@ -277,7 +277,7 @@ def main_worker(gpu, ngpus_per_node, args):
             if args.gpu is not None:
                 # best_acc1 may be from a checkpoint from a different GPU
                 best_acc1 = best_acc1.to(args.gpu)
-            model.load_state_dict(checkpoint['state_dict'])
+            model.load_state_dict(checkpoint['model'])
             optimizer.load_state_dict(checkpoint['optimizer'])
             print("=> loaded checkpoint '{}' (epoch {})"
                   .format(args.resume, checkpoint['epoch']))
