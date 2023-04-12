0ef94dbacd568d86537d424c954597febd78cf93
Abel Corrêa Dias <phd.abel@gmail.com>
2023-04-12 17:53:54 -0300

    Preprocessing MedNLI data.

--- preprocessing/preprocess_mednli.py
+++ preprocessing/preprocess_mednli.py
@@ -1,130 +1,131 @@
 import os
-import pickle
-import argparse
-import fnmatch
 import json
-
-from data_processor import MedNLIPreprocessor
-
-
-def preprocess_mednli_data(inputdir,
-                           targetdir,
-                           lowercase=False,
-                           ignore_punctuation=False,
-                           num_words=None,
-                           stopwords=[],
-                           labeldict={},
-                           bos=None,
-                           eos=None):
-    """
-    Preprocess the data from the MedNLI dataset so it can be used by the
-    ESIM model.
-    Compute a worddict from the train set, and transform the words in
-    the sentences of the corpus to their indices, as well as the labels.
-    The preprocessed data is saved in pickled form in some target directory.
-
-    Args:
-        inputdir: The path to the directory containing the MedNLI dataset.
-        targetdir: The path to the directory where the preprocessed data
-            must be saved.
-        lowercase: Boolean value indicating whether to lowercase the premises
-            and hypotheseses in the input data. Defautls to False.
-        ignore_punctuation: Boolean value indicating whether to remove
-            punctuation from the input data. Defaults to False.
-        num_words: Integer value indicating the size of the vocabulary to use
-            for the word embeddings. If set to None, all words are kept.
-            Defaults to None.
-        stopwords: A list of words that must be ignored when preprocessing
-            the data. Defaults to an empty list.
-        bos: A string indicating the symbol to use for beginning of sentence
-            tokens. If set to None, bos tokens aren't used. Defaults to None.
-        eos: A string indicating the symbol to use for end of sentence tokens.
-            If set to None, eos tokens aren't used. Defaults to None.
-    """
-    if not os.path.exists(targetdir):
-        os.makedirs(targetdir)
-
-    train_file = ""
-    dev_file = ""
-    test_file = ""
-    for file_ in os.listdir(inputdir):
-        if fnmatch.fnmatch(file_, "*train*"):
-            train_file = file_
-        elif fnmatch.fnmatch(file_, "*dev*"):
-            dev_file = file_
-        elif fnmatch.fnmatch(file_, "*test*"):
-            test_file = file_
-
-    preprocessor = MedNLIPreprocessor(lowercase=lowercase,
-                                      ignore_punctuation=ignore_punctuation,
-                                      num_words=num_words,
-                                      stopwords=stopwords,
-                                      labeldict=labeldict,
-                                      bos=bos,
-                                      eos=eos)
-
-    # -------------------- Train data preprocessing -------------------- #
-    print(20 * "=", " Preprocessing train set ", 20 * "=")
-    print("\t* Reading data...")
-    data = preprocessor.read_data(os.path.join(inputdir, train_file))
-
-    print("\t* Computing worddict and saving it...")
-    preprocessor.build_worddict(data)
-    with open(os.path.join(targetdir, "worddict.pkl"), "wb") as pkl_file:
-        pickle.dump(preprocessor.worddict, pkl_file)
-
-    print("\t* Saving result...")
-    with open(os.path.join(targetdir, "train_data.pkl"), "wb") as pkl_file:
-        pickle.dump(data, pkl_file)
-
-    # -------------------- Validation data preprocessing -------------------- #
-    print(20 * "=", " Preprocessing dev set ", 20 * "=")
-    print("\t* Reading data...")
-    data = preprocessor.read_data(os.path.join(inputdir, dev_file))
-
-    print("\t* Saving result...")
-    with open(os.path.join(targetdir, "dev_data.pkl"), "wb") as pkl_file:
-        pickle.dump(data, pkl_file)
-
-    # -------------------- Test data preprocessing -------------------- #
-    print(20 * "=", " Preprocessing test set ", 20 * "=")
-    print("\t* Reading data...")
-    data = preprocessor.read_data(os.path.join(inputdir, test_file))
-
-    print("\t* Saving result...")
-    with open(os.path.join(targetdir, "test_data.pkl"), "wb") as pkl_file:
-        pickle.dump(data, pkl_file)
+import re
+import fnmatch
+import pandas as pd
+from argparse import ArgumentParser
+import uuid
 
 
-if __name__ == "__main__":
-    default_config = "../config/mednli_preprocessing.json"
+def get_dataframe(filename, label_list=['entailment', 'contradiction']):
+    __uuid, iid, premise, hypothesis, label = [], [], [], [], []
+    with open(filename, 'r', encoding='utf-8') as input_data:
+        for line in input_data.readlines():
+            instance = json.loads(line)
 
-    parser = argparse.ArgumentParser(description="Preprocess the MedNLI dataset")
-    parser.add_argument(
-        "--config",
-        default=default_config,
-        help="Path to a configuration file for preprocessing MedNLI"
-    )
+            if instance['gold_label'] not in label_list:
+                continue
+
+            __uuid.append(str(uuid.uuid4()))
+            iid.append(instance['pairID'])
+            premise.append(instance['sentence1'])
+            hypothesis.append(instance['sentence2'])
+            label.append(instance['gold_label'])
+
+    return pd.DataFrame({'uuid': __uuid, 'iid': iid, 'premise': premise, 'hypothesis': hypothesis,
+                         'class_label': label})
+
+
+def __remove_artifacts(mednli_dataframe):
+    pattern = "\[\*\*.*?\*\*\]"
+    extra_info = dict(uuid=list(), iid=list(), hypothesis=list(), original_premise=list(), artifacts=list(),
+                      spans=list(), masks=list(), class_label=list())
+
+    def get_mask(artifact):
+        """
+        A very straightforward way to replace artifacts with a mask token.
+        Args:
+            artifact: The artifact to be replaced.
+
+        Returns: The mask token.
+
+        """
+        if 'ospital' in artifact:
+            return '<ORG>'
+        elif 'ocation' in artifact:
+            return '<LOC>'
+        elif 'Name' in artifact or 'name' in artifact:
+            return '<PERSON>'
+        elif 'Date' in artifact or 'Month' in artifact or 'Year' in artifact:
+            return '<DATE>'
+        else:
+            return '<MASK>'
+
+    for i, row in mednli_dataframe.iterrows():
+        artifacts, spans, masks = [], [], []
+        for j, patt in enumerate(re.finditer(pattern, row.premise)):
+            artifacts.append(patt.groups())
+            spans.append(patt.span())
+            masks.append(get_mask(patt.groups()))
+
+        extra_info['uuid'].append(row['uuid'])
+        extra_info['iid'].append(row['iid'])
+        extra_info['hypothesis'].append(row['hypothesis'])
+        extra_info['original_premise'].append(row['premise'])
+        extra_info['artifacts'].append(artifacts)
+        extra_info['spans'].append(spans)
+        extra_info['masks'].append(masks)
+        extra_info['class_label'].append(row['class_label'])
+
+    tmp = pd.DataFrame(extra_info)
+    premises = list()
+    for i, row in tmp.iterrows():
+        if len(row.spans) > 0:
+            spans = row.spans.copy()
+            spans.reverse()
+            x = row.original_premise
+            for j, span in enumerate(spans):
+                x = x[:span[0]] + row.masks[len(row.masks) - 1 - j] + x[span[1]:]
+            premises.append(x)
+        else:
+            premises.append(row.original_premise)
+    tmp['premise'] = premises
+    return tmp
+
+
+def prepare_two_labeled_dataset(filename):
+    return __remove_artifacts(get_dataframe(filename))
+
+
+def preprocess_data(input_dir, target_dir):
+
+    train_file, test_file, dev_file = None, None, None
+    for file_ in os.listdir(input_dir):
+        if fnmatch.fnmatch(file_, '*train*.jsonl'):
+            train_file = os.path.join(input_dir, file_)
+        elif fnmatch.fnmatch(file_, '*test*.jsonl'):
+            test_file = os.path.join(input_dir, file_)
+        elif fnmatch.fnmatch(file_, '*dev*.jsonl'):
+            dev_file = os.path.join(input_dir, file_)
+
+    mednli_train_df = prepare_two_labeled_dataset(train_file)
+    mednli_val_df = prepare_two_labeled_dataset(dev_file)
+    mednli_test_df = prepare_two_labeled_dataset(test_file)
+
+    mednli_train_df.to_pickle(os.path.join(target_dir, 'mednli_2L_train.pkl'))
+    mednli_val_df.to_pickle(os.path.join(target_dir, 'mednli_2L_val.pkl'))
+    mednli_test_df.to_pickle(os.path.join(target_dir, 'mednli_2L_test.pkl'))
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default=os.path.join('../datasets/raw/mednli'),
+                        help='Path to the directory containing the raw MedNLI dataset.')
+    parser.add_argument('--target_dir', type=str, default=os.path.join('../datasets/preprocessed/mednli'),
+                        help='Path to the directory where the preprocessed MedNLI dataset will be stored.')
+
     args = parser.parse_args()
 
-    script_dir = os.path.dirname(os.path.realpath(__file__))
+    if not os.path.exists(args.target_dir):
+        os.makedirs(args.target_dir)
 
-    if args.config == default_config:
-        config_path = os.path.join(script_dir, args.config)
-    else:
-        config_path = args.config
+    return args
 
-    with open(os.path.normpath(config_path), "r") as cfg_file:
-        config = json.load(cfg_file)
 
-    preprocess_mednli_data(
-        os.path.normpath(os.path.join(script_dir, config["data_dir"])),
-        os.path.normpath(os.path.join(script_dir, config["target_dir"])),
-        lowercase=config["lowercase"],
-        ignore_punctuation=config["ignore_punctuation"],
-        num_words=config["num_words"],
-        stopwords=config["stopwords"],
-        labeldict=config["labeldict"],
-        bos=config["bos"],
-        eos=config["eos"]
-    )
+def __main__():
+    args = get_args()
+    preprocess_data(args.input_dir, args.target_dir)
+
+
+if __name__ == '__main__':
+    __main__()
